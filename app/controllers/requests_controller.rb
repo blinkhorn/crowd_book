758ba5470d93a275f69b9f28ccfafad215a6ef7c
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2017-09-15 12:26:18 -0400

    initialized controllers and views

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,13 @@
+class RequestsController < ApplicationController
+  def edit
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,27 @@
+Rails.application.routes.draw do
+  get 'venues/edit'
+
+  get 'venues/index'
+
+  get 'venues/new'
+
+  get 'venues/show'
+
+  get 'requests/edit'
+
+  get 'requests/index'
+
+  get 'requests/new'
+
+  get 'requests/show'
+
+  get 'users/edit'
+
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/show'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
