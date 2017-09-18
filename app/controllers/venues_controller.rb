49e095d1b935680998791075fd27f5f9a79cf4b1
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2017-09-18 09:41:02 -0400

    added venues controller

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,36 +1,21 @@
 class VenuesController < ApplicationController
 
-  # def show
-  #   @venue = Venue.find(params[:id])
-  # end
-  #
-  # def new
-  #   @venue = Venue.new
-  # end
-  #
-  # def create
-  #   @venue = Venue.create!(user_params)
-  #   redirect_to "/users"
-  # end
-  #
-  # def edit
-  #   @user = User.find(params[:id])
-  # end
-  #
-  # def update
-  #   @user = Venue.find(params[:id])
-  #   @user.update(user_params)
-  #   redirect_to "/users/#{@user.id}"
-  # end
-  #
-  # def destroy
-  #   @user = User.find(params[:id])
-  #   @user.destroy
-  #   redirect_to "/users"
-  # end
-  #
-  # private
-  # def user_params
-  #   params.require(:user).permit(:username, :name, :password_digest, :email) #maybe not password_digest
-  # end
+  def show
+    @venue = Venue.find(params[:id])
+  end
+
+  def edit
+    @venue = Venue.find(params[:id])
+  end
+
+  def update
+    @venue = Venue.find(params[:id])
+    @venue.update(venue_params)
+    redirect_to "/venues/#{@venue.id}"
+  end
+
+  private
+  def venue_params
+    params.require(:venue).permit(:name)
+  end
 end
