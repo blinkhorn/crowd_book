2787bfa018b860ed023274b124a057fcff5cff52
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2017-09-18 11:12:57 -0400

    created venues views

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,14 @@
 Rails.application.routes.draw do
-  get 'venues/edit'
-
-  get 'venues/index'
-
-  get 'venues/new'
-
-  get 'venues/show'
-
-  get 'requests/edit'
-
-  get 'requests/index'
-
-  get 'requests/new'
-
-  get 'requests/show'
-
-  get 'users/edit'
-
-  get 'users/index'
-
-  get 'users/new'
-
-  get 'users/show'
-
+  root to: 'users#index'
+  root to: 'venues#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users do
+    resources :requests
+  end
+
+  resources :venues do
+    resources :requests
+  end
+
 end
