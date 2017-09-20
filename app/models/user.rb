cab396d7885cc0bf5054e0a74e54dce8243a6038
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2017-09-20 09:00:44 -0400

    added user + venue validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :requests, dependent: :destroy
+  validates :username, :name, :password_digest, :email, presence: true
+  validates :username, :email, uniqueness: true
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,3 +1,4 @@
 class Venue < ApplicationRecord
   has_many :requests, dependent: :destroy
+  validates :name, presence: true
 end
