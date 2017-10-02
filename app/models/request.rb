1a439e730daaee618c4730acccba3e65af2cc8f6
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2017-10-02 13:21:51 -0500

    updated migrate files; edited models

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,5 +2,4 @@ class Request < ApplicationRecord
   belongs_to :user
   has_many :venues#, dependent: :destroy
   validates :name, presence: true
-  validates :name, :soundcloud, :facebook, uniqueness: { case_sensitive: false }
 end
--- db/migrate/20170915195117_create_venues.rb
+++ db/migrate/20170915195117_create_venues.rb
@@ -2,7 +2,8 @@ class CreateVenues < ActiveRecord::Migration[5.1]
   def change
     create_table :venues do |t|
       t.string :name
-      
+      t.references :users, null: false, index:true
+      t.references :requests, null: false, index:true
       t.timestamps
     end
   end
